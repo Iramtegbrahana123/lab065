fd1f09954d2508031bc289389437fcc17640b9fc
Iramtegbrahana123 <sayednsrddn@gmail.com>
2025-05-16 14:45:41 +0530

    Add files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,21 @@
+
+
+public class Fibonaci {
+   
+    public static void main(String[] args) {
+        int n = 10; // Change this number to print more or fewer terms
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(first + " ");
+
+            // Compute the next term
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+
